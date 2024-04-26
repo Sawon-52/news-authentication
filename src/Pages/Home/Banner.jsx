ff34196696051a3583b3c0215a9ff149f032a5a4
Sawon-52 <mehedihasansawon.52@gmail.com>
2024-04-26 21:51:38 +0600

    create banner and atteched it in Home page

---
+++ src/Pages/Home/Banner.jsx
@@ -0,0 +1,10 @@
+const Banner = () => {
+  return (
+    <div className="flex flex-col md:flex-row gap-4 bg-gray-200 p-5 items-center my-5">
+      <button className="p-3 w-36 bg-pink-600 text-white ">Latest</button>
+      <h2 className="text-xl font-bold">Match Highlights: Germany vs Spain — as it happened ! Match Highlights: Germany vs Spain Germany vs Spain as... </h2>
+    </div>
+  );
+};
+
+export default Banner;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,11 +2,13 @@ import Header from "../../Shared/Header/Header";
 import LeftSideNavbar from "../../Shared/LeftSideNavbar/LeftSideNavbar";
 import Navber from "../../Shared/Navber/Navber";
 import RightSideNavber from "../../Shared/RightSideNavber/RightSideNavber";
+import Banner from "./Banner";
 
 const Home = () => {
   return (
     <div className="md:w-4/5 mx-auto">
       <Header></Header>
+      <Banner></Banner>
       <Navber></Navber>
       <div className="grid grid-cols-1 md:grid-cols-4  gap-5">
         <div className="border-2">
