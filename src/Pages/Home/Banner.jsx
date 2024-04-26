b32a04e291fdedfe1339bfbb62d3e1a91f718c5d
Sawon-52 <mehedihasansawon.52@gmail.com>
2024-04-26 22:22:40 +0600

    created navber and using marquee in banner section

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -1,8 +1,12 @@
+import Marquee from "react-fast-marquee";
+
 const Banner = () => {
   return (
     <div className="flex flex-col md:flex-row gap-4 bg-gray-200 p-5 items-center my-5">
       <button className="p-3 w-36 bg-pink-600 text-white ">Latest</button>
-      <h2 className="text-xl font-bold">Match Highlights: Germany vs Spain — as it happened ! Match Highlights: Germany vs Spain Germany vs Spain as... </h2>
+      <Marquee pauseOnHover={true} speed={100}>
+        <h2 className="text-xl font-bold">Match Highlights: Germany vs Spain — as it happened ! Match Highlights as... </h2>
+      </Marquee>
     </div>
   );
 };
--- src/Shared/Navber/Navber.jsx
+++ src/Shared/Navber/Navber.jsx
@@ -1,7 +1,45 @@
+import { NavLink } from "react-router-dom";
+import { FaUserCircle } from "react-icons/fa";
+
 const Navber = () => {
+  const links = (
+    <>
+      <li className="">
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li className="">
+        <NavLink to="/about">About</NavLink>
+      </li>
+      <li className="">
+        <NavLink to="/career">Career</NavLink>
+      </li>
+    </>
+  );
   return (
-    <div>
-      <h2>Navber</h2>
+    <div className="my-8">
+      <div className="navbar bg-base-100 px-0">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+              </svg>
+            </div>
+            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 ">
+              {links}
+            </ul>
+          </div>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 gap-5">{links}</ul>
+        </div>
+        <div className="navbar-end gap-2">
+          <div className="text-4xl">
+            <FaUserCircle></FaUserCircle>
+          </div>
+          <a className="bg-gray-700 text-white text-md p-2 px-5 cursor-pointer">Login</a>
+        </div>
+      </div>
     </div>
   );
 };
